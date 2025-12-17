b71f2aa5b57664b7fed9f192c7da4a2149a96171
Denise Worthen <denise.worthen@noaa.gov>
2025-12-17 14:45:47 -0500

    add generation of 1/12 regional domain for weight generation

--- reg_tests/cpld_gridgen/cpld_gridgen.sh
+++ reg_tests/cpld_gridgen/cpld_gridgen.sh
@@ -186,7 +186,30 @@ if [ $RESNAME = 008 ]; then
         $APRUN -n 1 ncremap -g ${OUTDIR_PATH}/rect.0p25_SCRIP.nc -G latlon=721,1440#lon_typ=grn_ctr#lat_typ=cap
         $APRUN -n 1 ncremap -g ${OUTDIR_PATH}/rect.0p125_SCRIP.nc -G latlon=1440,2880#lon_typ=grn_ctr#lat_typ=cap
     fi
+
+    #if [ $MAKE_REGIONAL == .true. ]; then
+        # Define spacing value
+        dx=$(echo "scale=10; 1/12" | bc)
+
+        # Define center grid points at corners of domain
+        slon=-210
+        elon=-50
+
+        slat=-20
+        elat=70
+
+        # Calculate snwe bounds
+        ll_lat=$(echo "$slat - $dx/2" | bc -l)
+        ur_lat=$(echo "$elat + $dx/2" | bc -l)
+
+        ll_lon=$(echo "$slon - $dx/2" | bc -l)
+        ur_lon=$(echo "$elon + $dx/2" | bc -l)
+
+        #atmos river mom6 domain; 1/12 deg rectilinear
+        ncremap -g ar.0p08.SCRIP.nc -G latlon=1081,1920#snwe=$ll_lat,$ur_lat,$ll_lon,$ur_lon#lat_typ=uni
+    #fi
 fi
+
 edit_namelist < grid.nml.IN > grid.nml
 $APRUN ./cpld_gridgen
 
